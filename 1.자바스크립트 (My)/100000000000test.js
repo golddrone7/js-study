df3f4348d3517399a8df7a399519edfb2c4abfd5
golddrone7 <95079688+golddrone7@users.noreply.github.com>
2023-03-17 08:57:19 +0900

    폴더이름변경

---
+++ 1.자바스크립트 (My)/100000000000test.js
@@ -0,0 +1,5 @@
+let firstName = "";
+let lastName = "김자바";
+let nickName = "";
+
+alert(firstName || lastName || nickName || "익명") ;
--- 1.자바스크립트 기초/100000000000test.js
+++
@@ -1,7 +0,0 @@
-var name = prompt('이름이 무엇인가??', '감자바');
-var isTure = confirm(`${name}이 맞더냐?`);
-if(isTure)
-    alert(name);
-else
-    alert('아닙니다');
-    
