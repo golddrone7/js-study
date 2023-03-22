970a5fec3633c942ef6cdf180cb94cd565396799
golddrone7 <95079688+golddrone7@users.noreply.github.com>
2023-03-22 17:41:08 +0900

    3월22일 마무리

--- 5.ES6(My)/100.콜백연습하기.js
+++ 5.ES6(My)/100.콜백연습하기.js
@@ -1,27 +1,156 @@
+const userList = [{
+    account: 'abc1234',
+    userName: '대길이',
+    job: '추노',
+    address: '서울',
+    hobbys: ['수영', '축구', '테니스']
+  },
+  {
+    account: 'banana',
+    userName: '빠나나',
+    job: '과일',
+    address: '서울',
+    hobbys: ['푸드파이팅', '테니스']
+  },
+  {
+    account: 'park1234',
+    userName: '주차왕',
+    job: '발렛파킹',
+    address: '경기',
+    hobbys: ['족구', '축구', '테니스', '영화감상']
+  },
+  {
+    account: 'fire',
+    userName: '불꽃남자카리스마',
+    job: '게이머',
+    address: '서울',
+    hobbys: ['독서', '테니스']
+  },
+];
 
-
-function showMultipleNumber(n, multiNumber){
-    for(let i=1; i<=n; i++){
-        if(i% multiNumber ===0 ) console.log(i);
+function alertUserInfo(wannaSay){
+    for(const user of userList){
+        wannaSay(user);
     }
 }
 
+alertUserInfo(u => console.log(`나는 ${u.userName}님이다!`));
 
-function showNumber(n, message){
-    for(let i =1; i<=n; i++){
-        if(message(i)){
-            console.log(i);
+const numbers =[10,20, 30, 40];
+
+let total = 0;
+numbers.forEach(n => total += n);
+console.log(`총합: ${total}`);
+
+function filter2Hobby(){
+    const filteredArray = [];
+
+    for(const user of userList){
+        if(user.hobbys.length === 2){
+            filteredArray.push(user);
         }
     }
+    return filteredArray;
 }
 
+// 경기 사는 회원들만 
+// 필터링해서 새로운 배열에 담아줘
 
-// showNumber(50, (num) => {
-//     return num % 6 === 0 && num % 12 !==0;
-// });
+function filterUserLivedInSeoul() {
+    const filteredArray = [];
+
+    for(const user of userList){
+        if(user.address === '경기'){
+            filteredArray.push(user);
+        }
+    }
+    return filteredArray;
+}
+
+console.log(filterUserLivedInSeoul());
+
+console.log('=========================================');
+
+// 특정 필터조건에 의해 필터링하는 함수
+function filter(condition){
+    const filteredArray = [];
+
+    for(const user of userList){
+        if(condition(user)){
+            filteredArray.push(user);
+        }
+    }
+    return filteredArray;
+}
+
+console.log(
+    filter(guest=>guest.address==='서울') // [{5},{5},{5}]  
+    .map(seoul => ({                        // [{2},{2},{2}]
+        name : seoul.userName,
+        account : seoul.account
+    })).forEach(u=>console.log(`이름과 아이디가 뭐니? ${u.name}, ${u.account}`)));    // [{2},{2},{2}]
 
 
-showNumber(50, function(num){
-    return num%6===0 && num%12!==0;
-});
+console.log('클로저 연습하기!!!!!!!!!');
+
+// let count = 0;
+
+// const increase = () => ++count;
+// console.log(increase());
+// console.log(increase());
+// count = 999;
+// console.log(increase());
+
+
+
+// const increase = (() => {
+//     let count = 0;
+//     return () => ++count;
+// })();
+
+// const decrease = (()=>{
+//     let count = 0;
+//     return ()=>--count;
+// })();
+
+// console.log(increase());
+// console.log(increase());
+// console.log(decrease());
+
+
+// function counter(){
+//     let count = 0;
+//     const increase = () => ++count;
+//     const decrease = () => --count;
+
+//     return {
+//         increase, decrease
+//     };
+// }
+// const count = counter();
+
+
+// const increase = count.increase;
+// const decrease = count.decrease;
+
+// console.log(increase());
+// console.log(increase());
+// console.log(increase());
+// console.log(decrease());
+
+const counter_ = (() => {
+    let count = 0;
+    return callback => count = callback(count);
+})();
+
+console.log(counter_(c=>++c));
+console.log(counter_(c=>c+=3));
+console.log(counter_(c=>c**=2));
+
+
+
+
+
+
+
 
--- 5.ES6(My)/8.배열 구조분해할당.js
+++ 5.ES6(My)/8.배열 구조분해할당.js
@@ -1,6 +1,4 @@
-
-
-const userNames = ['김철수', '김강찬', '박영희'];
+const userNames = ['김철수', '강감찬', '박영희'];
 
 const [kim, kang, park] = userNames;
 
@@ -9,24 +7,58 @@ console.log(kang);
 console.log(park);
 
 
-const [firstObj] = [{ id : 1 }, { id : 2 }, { id : 3}]
-                .filter(obj => obj.id >= 2);
+const [firstObj] = [{
+    id: 1
+}, {
+    id: 2
+}, {
+    id: 3
+}]
+.filter(obj => obj.id >= 2);
+
 console.log(firstObj);
 
 
 // 변수 교환하기
-let first = 10, second = 20;
-[first, second] = [second, first];
+let first = 10,
+    second = 20;
+
+[second, first] = [first, second];
+
 console.log(first);
 console.log(second);
 
+
 // 앞에 2개는 변수에 각각 넣고 나머지는 다시 배열로 묶고싶다
-const numbers = [1,3,5,7,9,11];
+const numbers = [1, 3, 5, 7, 9, 11];
+
+
 const [one, three, ...others] = numbers;
 
+/*
+const copyNumbers = numbers.slice();
 
-// log
-console.log('=======================');
-const foods = ['감튀', 'DPQC, '징거버거'];
+const one = copyNumbers.shift();
+const three = copyNumbers.shift();
+const others = copyNumbers;
+*/
 
-const copyFoods = [...foods]; //  실제 데이터 복사
+console.log('================');
+console.log(one);
+console.log(three);
+console.log(others);
+console.log(numbers);
+
+
+// 스프레드로 배열 간편복사
+console.log('==================');
+
+const foods = ['감튀', 'DQPC', '징거버거'];
+
+// const copyFoods = foods; // 주소 복사
+const copyFoods = [...foods, '사이다', '쥬스']; // 실제 데이터 복사
+
+foods[0] = '콜라';
+
+console.log(foods);
+console.log(copyFoods);
